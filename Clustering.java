3a75c34997a0c1ead6d58fcc5c6dff38567bf419
Amit Kawad <37235516+AmitKawad@users.noreply.github.com>
2018-03-10 14:28:54 +0530

    Update Clustering.java

--- Clustering.java
+++ Clustering.java
@@ -18,31 +18,41 @@ public class Clustering
 			int k=0;
 			
 			
-			ArrayList<Integer> cluster1 = new ArrayList<Integer>();
+			ArrayList<Integer> cluster1 = new ArrayList<Integer>();  /*Array list for cluster since the number of 
+											elements are not deterministic*/
 			ArrayList<Integer> cluster2 = new ArrayList<Integer>();
-			int[] myarray={3, 6, 10, 16, 19, 23, 30, 11, 25};
-			int random=(int)(Math.random()*8);
+			int[] myarray={3, 6, 10, 16, 19, 23, 30, 11, 25};       // Observations
+			int random=(int)(Math.random()*8);                      //Generate Random number in the range of size of the array//
 			
 			int random2=(int)(Math.random()*8);
 			while(random==random2)
 			{
-				random=(int)(Math.random()*8);
+				random=(int)(Math.random()*8);		/* If both the random numbers generated happen to be 
+									the same,keep
+									generating a pair of random numbers until they are distinct*/
+										
 				random2=(int)(Math.random()*8);
 			}
 			
-			m1=myarray[random];
+			m1=myarray[random];				/*Assign the random number needed for the algorith
+									to the index of the array by using the random number
+									generated above*/
 			m2=myarray[random2];
 
-			System.out.println(m1);
+			System.out.println(m1);					//RANDOM ELEMENTS from the Array//
 			System.out.println(m2);
 
-			 for(i=0;i<myarray.length;i++)
+			 for(i=0;i<myarray.length;i++)			/* Run a loop for the entire length of the  
+			 							array and compare every element 
+										with both the random numbers generated 
+											from the array*/
 			 {
 			 		int sum1=Math.abs(myarray[i]-m1);
 			 		int sum2=Math.abs(myarray[i]-m2);
 			 		if(sum1<=sum2)
 			 		{
-			 			cluster1.add(myarray[i]);
+			 			cluster1.add(myarray[i]);		/*Determine if the element belongs
+											to cluster1 or cluster2 */
 			 			j++;
 			 		}
 			 		else
@@ -57,7 +67,7 @@ public class Clustering
 			 System.out.println("Printing cluster 2");
 			 System.out.println(cluster2);
 
-			 	int size1=cluster1.size();
+			 	int size1=cluster1.size();				//determine the size of the cluster
 			 	int size2=cluster2.size();
 
 			 	for(i=0;i<size1;i++)
@@ -73,7 +83,8 @@ public class Clustering
 			 	}
 			 	avg1=(double)summ1/size1;
 			 	avg2=(double)summ2/size2;
-			 	System.out.println("Average of cluster 1 is "+avg1);
+			 	System.out.println("Average of cluster 1 is "+avg1);		/* print averages of both 
+												clusters*/
 			 	System.out.println("Average of cluster 2 is "+avg2);
 			 	
 			 	
