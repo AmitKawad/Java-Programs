58beaecfae8747d7d92a76978db2dda4a1657540
Amit Kawad <37235516+AmitKawad@users.noreply.github.com>
2018-03-17 10:33:45 +0530

    Add files via upload
    Berkley's Clock Synchronization Algorithm in Java.

---
+++ Berkley.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.lang.Math;
+public class Berkley
+{
+	public static void main(String args[])
+	{
+		int hr,min,sum=0;
+		int n,i;
+		Scanner scan =new Scanner(System.in);
+		System.out.println("Enter the number of Systems");
+		n=scan.nextInt();
+		int[] h=new int[n];
+		int[] m=new int[n];
+		int[] diff=new int[n];
+		int[] hrs =new int[n];
+		int random=(int)(Math.random()*n);
+
+
+		System.out.println("Enter hours for each system");
+		for(i=0;i<n;i++)
+		{
+			h[i]=scan.nextInt();
+		}
+		System.out.println("Enter minutes for each system");
+		for(i=0;i<n;i++)
+		{
+			m[i]=scan.nextInt();
+			
+		}
+		for(i=0;i<n;i++)
+		{
+			System.out.println("time for System "+i+"is "+h[i]+":"+m[i]);
+		}
+		
+		
+		for(i=1;i<n;i++)									
+		{
+			if(h[i]<h[0])
+			{
+				diff[i]= (-1)*((60-m[i])+m[0]);
+			}
+			else
+			{
+				diff[i]=(-1)*(m[0]-m[i]);           
+			}
+			
+			sum=sum+diff[i];
+
+				
+		}
+		System.out.println("Sum is "+sum);
+		
+		int avg=Math.abs(sum/n);
+		System.out.println("average is "+avg);
+
+		
+		m[0]=m[0]+avg;
+
+		System.out.println("Time for System 0 is "+h[0]+":"+m[0]);
+		for(i=1;i<n;i++)
+		{
+			m[i]=m[0];
+			h[i]=h[0];
+			System.out.println("time for System "+i+"is "+h[i]+":"+m[i]);
+		}
+
+
+
+	}
+
+}
